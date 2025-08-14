53b9e62be4a2d177f8ee44a56d79bd1752a38cc6
eikecangussu <210372870+eikecangussu@users.noreply.github.com>
2025-08-14 16:21:12 -0300

    Pasta com arquivos atualizados

--- ED1_Grupo9_[EikeCangussu][GabrielAmaral][MatheusAlves][GuilhermeAnjos]/pilha.c
+++ ED1_Grupo9_[EikeCangussu][GabrielAmaral][MatheusAlves][GuilhermeAnjos]/pilha.c
@@ -25,42 +25,42 @@ Pilha *esvazia(Pilha *p){
 }
 
 bool vazia(Pilha *p){
-    if(p->topo == NULL) return true;
+    if(p->topo == NULL) return true; //Verifica se a pilha é vazia
     else return false;
 }
 
 void empilha(Pilha *p, void *chave){
-    No *novo = criaNo(chave);
-    novo->prox = p->topo;
-    p->topo = novo;
-    p->tamanho++;
+    No *novo = criaNo(chave); // Aloca o novo nó e atribui o valor a nova chave
+    novo->prox = p->topo; // Aponta o novo nó para o topo atual
+    p->topo = novo; // Torna o novo nó o topo
+    p->tamanho++; // Aumenta o tamanho da pilha
 }
 
 void *desempilha(Pilha *p){
-    void *removido = NULL;
-    if(p->topo != NULL){
-        removido = p->topo->ch;
-        No *aux = p->topo;
-        p->topo = p->topo->prox;
-        free(aux);
+    void *removido = NULL; // Inicializa a variável removido, que guarda o nó que será removido da pilha
+    if(p->topo != NULL){ // Verifica se a pilha está vazia
+        removido = p->topo->ch; // Guarda a chave do topo atual que será removida
+        No *aux = p->topo; // Guarda o nó do topo em uma variável auxiliar
+        p->topo = p->topo->prox; // Atualiza o topo da lista para o nó abaixo dele
+        free(aux); // Libera a memória do antigo topo
     }
     return removido;
 }
 
 int tamanho(Pilha *p){
-    return p->tamanho;
+    return p->tamanho; // Retorna o tamanho da pilha
 }
 
 void destroiPilha(Pilha *p){
-    if (p != NULL){
-        esvazia(p);
-        free(p);
+    if (p != NULL){ // Verifica se a a estrutura pilha está vazia
+        esvazia(p); // Esvazia a lista encadeada dentro da pilha liberando a memória
+        free(p); // Libera a mémoria da estrutura pilha
     }
 }
 
 void *mostrarTopo(Pilha *p){
-    if(vazia(p)){
-        return NULL;
+    if(vazia(p)){ // Verifica se o topo da pilha está vazio
+        return NULL; // Retorna NULL caso o topo da pilha está vazio
     }
-    return p->topo->ch;
+    return p->topo->ch; // Retorna a chave do topo caso o topo da pilha seja diferente de NULL
 }
